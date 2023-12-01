58a54daccaa335bb84e52add9bdae39f44526038
bingyanghuang0217 <91875227+bingyanghuang0217@users.noreply.github.com>
2023-12-01 14:27:38 +0000

    rail generate

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,17 +5,8 @@
 #  id         :integer          not null, primary key
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  fan_id     :integer
 #  photo_id   :integer
+#  user_id    :integer
 #
 class Like < ApplicationRecord
-
-  validates :photo_id, presence: true
-  validates :photo_id, uniqueness: { scope: ["fan_id"], message: "already liked" }
-  validates :fan_id, presence: true
-
-  belongs_to :fan, required: true, class_name: "User", foreign_key: "fan_id", counter_cache: true
-  belongs_to :photo, required: true, class_name: "Photo", foreign_key: "photo_id", counter_cache: true
-
 end
-
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,23 +3,14 @@
 # Table name: photos
 #
 #  id             :integer          not null, primary key
-#  caption        :text
+#  caption        :string
 #  comments_count :integer
 #  image          :string
 #  likes_count    :integer
+#  location       :string
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
 #  owner_id       :integer
 #
 class Photo < ApplicationRecord
-
-  validates :owner_id, presence: true
-  validates :image, presence: true
-
-  belongs_to :poster, required: true, class_name: "User", foreign_key: "owner_id", counter_cache: :own_photos_count
-  has_many  :comments, class_name: "Comment", foreign_key: "photo_id", dependent: :destroy
-  has_many  :likes, class_name: "Like", foreign_key: "photo_id", dependent: :destroy
-  has_many :commenters, through: :comments, source: :commenter
-  has_many :fans, through: :likes, source: :fan
-
 end
